da4b8dfd95fa3d8e9fe05e958b4b2f62cd9e9481
Mitchell Kates <mhkates@gmail.com>
2013-10-03 21:48:52 -0400

    login signup and product templates added

--- deviceapp/views.py
+++ deviceapp/views.py
@@ -12,3 +12,17 @@ def index(request):
 def search(request):
 	return render_to_response('search.html',context_instance=RequestContext(request))
 
+def login(request):
+	return render_to_response('login.html',context_instance=RequestContext(request))
+
+def signup(request):
+	return render_to_response('signup.html',context_instance=RequestContext(request))
+
+def addproduct(request):
+	return render_to_response('addproduct.html',context_instance=RequestContext(request))
+
+def forgotpassword(request):
+	return render_to_response('passwordreset.html',context_instance=RequestContext(request))
+
+def product(request):
+	return render_to_response('product.html',context_instance=RequestContext(request))
--- medapp/urls.py
+++ medapp/urls.py
@@ -1,19 +1,22 @@
 from django.conf.urls import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
     url(r'^$','deviceapp.views.index'),
-     url(r'^search','deviceapp.views.search'),
-    # url(r'^$', 'medapp.views.home', name='home'),
-    # url(r'^medapp/', include('medapp.foo.urls')),
-
+    url(r'^search','deviceapp.views.search'),
+    url(r'^login','deviceapp.views.login'),
+    url(r'^signup','deviceapp.views.signup'),
+    url(r'^addproduct','deviceapp.views.addproduct'),
+    url(r'^forgotpassword','deviceapp.views.forgotpassword'),
+    url(r'^product','deviceapp.views.product'),
+     
     # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )
