d826ae2af8d958accb56069a1aa19653b0b86e48
Mitchell Kates <mhkates@gmail.com>
2013-12-09 14:19:47 -0500

    updated static url

--- medapp/settings.py
+++ medapp/settings.py
@@ -183,14 +183,15 @@ ALLOWED_HOSTS = ['*']
 import os
 if os.path.abspath( __file__ ).split("/")[2] == 'alexanderkates':
 	LOCAL = True
+	STATIC_URL = '/static/'
 else:
 	import dj_database_url
 	DATABASES['default'] =  dj_database_url.config()
+	STATIC_URL = 'https://devicerock.s3.amazonaws.com/'
 
 	
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = 'staticfiles'
-STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
