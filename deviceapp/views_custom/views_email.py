d7e01b6c76bd631011979b28564778e8aac614b8
Mitchell Kates <mhkates@gmail.com>
2014-01-22 20:47:08 -0500

    working on emails

--- deviceapp/views_custom/views_email.py
+++ deviceapp/views_custom/views_email.py
@@ -18,25 +18,31 @@ from django.template.loader import render_to_string
 def sendwelcomeemail(request):
 	template_data = {'email_pagetitle':'VetCove',
 			'email_teaser':'Welcome to VetCove. The worlds largest online equipment marketplace',
-			'email_header':'Welcome to VetCove!'}
+			'email_header':'Welcome to VetCove!',
+			'STATIC_URL':settings.STATIC_URL}
 	subject = "Welcome to VetCove!"
 	plaintext_context = Context(autoescape=False)  # HTML escaping not appropriate in plaintext
 	text_body = render_to_string("email_templates/test_plain.txt", template_data, plaintext_context)
-	html_body = render_to_string("email_templates/test_email.html", template_data)
+	html_body = render_to_string("email_templates/test_email2.html", template_data)
 	msg = EmailMultiAlternatives(subject=subject, from_email="mhkates@gmail.com",to=["mkates@mit.edu"], body=text_body)
 	msg.attach_alternative(html_body, "text/html")
-	msg.send()
+	#msg.send()
 	return
 
 def testmail(request):
-	template_data = {'email_pagetitle':'VetCove',
+	question = Question.objects.get(id=1)
+	template_data = {'question':question,
+			'email_title':'VetCove',
 			'email_teaser':'Welcome to VetCove. The worlds largest online equipment marketplace',
-			'email_header':'Welcome to VetCove!'}
+			'email_header':'Welcome to VetCove!',
+			'STATIC_URL':settings.STATIC_URL,
+			'email_name':'Alex Kates'}
 	subject = "Welcome to VetCove!"
 	plaintext_context = Context(autoescape=False)  # HTML escaping not appropriate in plaintext
 	text_body = render_to_string("email_templates/test_plain.txt", template_data, plaintext_context)
-	html_body = render_to_string("email_templates/test_email.html", template_data)
+	html_body = render_to_string("email_templates/email_base.html", template_data)
 	msg = EmailMultiAlternatives(subject=subject, from_email="mhkates@gmail.com",to=["mkates@mit.edu"], body=text_body)
 	msg.attach_alternative(html_body, "text/html")
 	msg.send()
-	return HttpResponse("sent")
+	return render_to_response('email_templates/email_base.html',template_data,context_instance=RequestContext(request))
+
