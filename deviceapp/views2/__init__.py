c4cfcace38e11e78485ae29aefbb113cf11b37b7
Mitchell Kates <mhkates@gmail.com>
2014-01-26 10:15:39 -0500

    cool

--- deviceapp/views2/__init__.py
+++
@@ -1,7 +0,0 @@
-from views_general import *
-from views_account import *
-from views_product import *
-from views_search import *
-from views_sell import *
-from views_email import *
-from views_checkout import *
