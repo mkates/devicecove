2cc4a66adb3a889b0bc51a91a075946b2e7e7b41
Mitchell Kates <mhkates@gmail.com>
2014-02-26 20:40:49 -0500

    indent error

--- medapp/settings.py
+++ medapp/settings.py
@@ -229,8 +229,8 @@ else:
 	import dj_database_url
 	DATABASES['default'] =  dj_database_url.config()
 	STATIC_URL = 'https://devicerock.s3.amazonaws.com/'
-    HTTPS_SUPPORT = True
-	
+	HTTPS_SUPPORT = True
+    
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = 'staticfiles'
 
