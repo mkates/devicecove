df334362ed1ec9b2d1f9ed782b1ea382f96f0138
Mitchell Kates <mhkates@gmail.com>
2014-02-27 15:36:21 -0500

    admin css broken

--- medapp/settings.py
+++ medapp/settings.py
@@ -211,7 +211,7 @@ except Exception,e:
     pass
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = '/static'
+STATIC_ROOT = 'staticfiles'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
